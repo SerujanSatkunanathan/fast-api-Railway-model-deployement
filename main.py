4e3f1d67ecdbdf52a521af0aae0ffa0dcc9df8c6
SerujanSatkunanathan <144580005+SerujanSatkunanathan@users.noreply.github.com>
2024-09-09 08:58:55 +0530

    new

---
+++ main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import joblib
+
+class IrisInput(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
+
+model = joblib.load("trainedModel.pkl")
+
+
+target_names = ['setosa', 'versicolor', 'virginica']
+
+
+app = FastAPI()
+
+
+@app.post("/predict")
+async def predict(iris: IrisInput):
+    try:
+        
+        input_features = [[
+            iris.sepal_length,
+            iris.sepal_width,
+            iris.petal_length,
+            iris.petal_width
+        ]]
+        
+        
+        prediction = model.predict(input_features)
+        prediction_class = target_names[prediction[0]]
+        
+        return {"prediction": prediction_class}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
